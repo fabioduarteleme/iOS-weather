1c9969eea7e2c0828281086c64671904891f8b48
Fabio Leme <duarteleme@gmail.com>
2022-03-29 17:54:25 -0300

    Adiciona magic scroll e refatoração em geral

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var controller = new ScrollMagic.Controller();
+
+// //https://scrollmagic.io/examples/basic/responsive_duration.html
+
+// $(function () {
+
+//   new ScrollMagic.Scene({ triggerElement: "#pin1", duration: 0, offset: 60, triggerHook: 0.1, })
+//     .setPin("#current-city")
+//     .setClassToggle("#current-city", "sticky")
+//     .addIndicators()
+//     .addTo(controller);
+
+//   new ScrollMagic.Scene({ triggerElement: "#pin2", duration: 0, offset: 70, triggerHook: 0.3, })
+//     .setPin("#card")
+//     .setClassToggle("#current-city", "sticky")
+//     .addIndicators()
+//     .addTo(controller);
+// });
