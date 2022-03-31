645f3e10f21c056fe292586075654d6c6abcffa1
Fabio Leme <duarteleme@gmail.com>
2022-03-30 21:43:18 -0300

    Adiciona header fixo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-// var controller = new ScrollMagic.Controller();
+
+//https://github.com/alexfoxy/lax.js
+
+
+
+// let controller = new ScrollMagic.Controller();
 
 // //https://scrollmagic.io/examples/basic/responsive_duration.html
 
@@ -16,3 +21,29 @@
 //     .addIndicators()
 //     .addTo(controller);
 // });
+
+
+
+$(window).scroll(function(){
+    let scrollPage = $(window).scrollTop();
+    let rangeDetails = 300
+    //let rangeWeatherResume = 300
+
+    if(scrollPage >= 57){
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+    }
+
+    // if(scrollPage >= 183){
+    //     $('.weather-resume').addClass('fixed-resume');
+    // } else {
+    //     $('.weather-resume').removeClass('fixed-resume');
+    // }
+
+    $('.header-details').css("opacity", 1- scrollPage / rangeDetails * 3);
+    // $('.weather-resume').css("margin-top", 1- scrollPage / rangeWeatherResume * 200);
+
+    console.log(scrollPage)
+  });
+
